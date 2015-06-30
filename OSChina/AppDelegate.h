8dde486f9a7bbb47def1c5f32a9236cfeea8501d
bluesky0109 <18356099575@163.com>
2015-06-30 11:11:43 +0800

    导入初始空项目

---
+++ OSChina/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OSChina
+//
+//  Created by sky on 15/6/30.
+//  Copyright (c) 2015年 bluesky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
