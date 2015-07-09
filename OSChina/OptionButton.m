c1da08f06745c3e02d50403b1550ed999d202cbb
bluesky0109 <18356099575@163.com>
2015-07-09 09:32:05 +0800

    为中间键弹出的选项按钮添加对应图标

--- OSChina/OptionButton.m
+++ OSChina/OptionButton.m
@@ -11,7 +11,8 @@
 
 @interface OptionButton()
 
-@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UILabel     *titleLabel;
+@property (nonatomic, strong) UIImageView *imageView;
 
 @end
 
@@ -20,8 +21,13 @@
 - (instancetype)initWithTitle:(NSString *)title image:(UIImage *)image andColor:(UIColor *)color {
     
     if (self = [super init]) {
-        _button = [UIView new];
+        _button = [UIImageView new];
         _button.backgroundColor = color;
+        
+        _imageView = [UIImageView new];
+        _imageView.image = image;
+        _imageView.translatesAutoresizingMaskIntoConstraints = NO;
+        [_button addSubview:_imageView];
         
         _titleLabel = [UILabel new];
         _titleLabel.text = title;
@@ -38,10 +44,12 @@
 - (void)setLayout {
     
     for (UIView *view in self.subviews) {view.translatesAutoresizingMaskIntoConstraints = NO;}
-    NSDictionary *views = NSDictionaryOfVariableBindings(_button, _titleLabel);
+    NSDictionary *views = NSDictionaryOfVariableBindings(_button, _titleLabel,_imageView);
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-8-[_button]-8-[_titleLabel]-8-|" options:NSLayoutFormatAlignAllCenterX metrics:nil views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-8-[_button]-8-|" options:0 metrics:nil views:views]];
+    [_button addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-15-[_imageView]-15-|" options:0 metrics:nil views:views]];
+    [_button addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-15-[_imageView]-15-|" options:0 metrics:nil views:views]];
 }
 
 @end
