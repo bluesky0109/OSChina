e551cc876bca8b393bc4858d9ff1d5967be4aac9
bluesky0109 <18356099575@163.com>
2015-07-10 14:10:17 +0800

    添加OperationBar类

---
+++ OSChina/OperationBar.h
@@ -0,0 +1,19 @@
+//
+//  OperationBar.h
+//  OSChina
+//
+//  Created by sky on 15/7/10.
+//  Copyright (c) 2015年 bluesky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OperationBar : UIToolbar
+
+@property (nonatomic, strong) UIButton *modeSwitchButton;
+@property (nonatomic, strong) UIButton *commentsButton;
+@property (nonatomic, strong) UIButton *starButton;
+@property (nonatomic, strong) UIButton *shareButton;
+@property (nonatomic, strong) UIButton *reportButton;
+
+@end
