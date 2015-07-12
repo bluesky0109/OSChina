ad497636b3257464d81d7f85a495f4878e941a3e
bluesky <724432662@qq.com>
2015-07-12 20:09:23 +0800

    帖子列表添加默认的占位头像

--- OSChina/PostsViewController.m
+++ OSChina/PostsViewController.m
@@ -10,7 +10,7 @@
 #import "DetailsViewController.h"
 #import "PostsCell.h"
 #import "OSCPost.h"
-
+#import "UIImageView+Util.h"
 #import <SDWebImage/UIImageView+WebCache.h>
 
 static NSString *kPostCellID = @"PostCell";
@@ -59,7 +59,7 @@ static NSString *kPostCellID = @"PostCell";
         PostsCell *cell = [self.tableView dequeueReusableCellWithIdentifier:kPostCellID forIndexPath:indexPath];
         OSCPost *post = self.objects[indexPath.row];
         
-        [cell.portrait sd_setImageWithURL:post.portraitURL placeholderImage:nil options:0];
+        [cell.portrait loadPortrait:post.portraitURL];
         [cell.titleLabel setText:post.title];
         [cell.bodyLabel setText:post.body];
         [cell.authorLabel setText:post.author];
