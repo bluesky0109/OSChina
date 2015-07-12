36869a5698d56bdb8a41f93da910182d0042c20a
bluesky <724432662@qq.com>
2015-07-12 13:51:31 +0800

    取消自动刷新动画

--- OSChina/OSCObjsViewController.m
+++ OSChina/OSCObjsViewController.m
@@ -53,9 +53,10 @@
         return;
     }
     
-    //无数据或未加载完自动进入刷新
-    [self.refreshControl beginRefreshing];
-    [self.tableView setContentOffset:CGPointMake(0, self.tableView.contentOffset.y-self.refreshControl.frame.size.height) animated:YES];
+    //无数据或未加载完自动进入刷新 刷新动画
+//    [self.refreshControl beginRefreshing];
+//    [self.tableView setContentOffset:CGPointMake(0, self.tableView.contentOffset.y-self.refreshControl.frame.size.height) animated:YES];
+    
     [self fetchObjectsOnPage:0 refresh:YES];
 }
 
