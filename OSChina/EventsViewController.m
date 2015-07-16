b87f4d693232da0b6bd9d0f6a239aa2c9f302c64
bluesky0109 <18356099575@163.com>
2015-07-16 15:54:14 +0800

    修改好友圈里的一些字体的大小和颜色配置

--- OSChina/EventsViewController.m
+++ OSChina/EventsViewController.m
@@ -131,6 +131,7 @@ static NSString * const kEventCellID = @"EventCell";
     if (indexPath.row < self.objects.count) {
         OSCEvent *event = self.objects[indexPath.row];
         
+        self.label.font = [UIFont systemFontOfSize:15];
         [self.label setAttributedText:[Utils emojiStringFromRawString:event.message]];
         CGSize size = [self.label sizeThatFits:CGSizeMake(tableView.frame.size.width - 51, MAXFLOAT)];
         CGFloat height = size.height + 24 + [UIFont systemFontOfSize:14].lineHeight;
