0ca0dd1f7bd15cef892bcd76731ede66e5be39bc
bluesky0109 <18356099575@163.com>
2015-07-20 14:26:14 +0800

    修复活动内容有时不显示的问题

--- OSChina/ActivityDetailsViewController.m
+++ OSChina/ActivityDetailsViewController.m
@@ -68,7 +68,10 @@
              commentsCountButton.badgePadding = 1;
              commentsCountButton.badgeBGColor = [UIColor colorWithHex:0x24a83d];
              
-             [self.tableView reloadData];
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 [self.tableView reloadData];
+             });
+
          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
              NSLog(@"wrong");
          }];
@@ -168,7 +171,7 @@
         case 1:
             return 35;
         case 2:
-            return _webViewHeight + 30;
+            return _isLoadingFinished? _webViewHeight + 30 : 400;
         default:
             return 0;
     }
